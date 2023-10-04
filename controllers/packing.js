89b701c04de55cd79c0594d98d821e2239c1af89
Elizabeth Cristina <lizzy.cristina@gmail.com>
2023-10-04 13:42:12 -0500

    Change packing.js form

--- controllers/packing.js
+++ controllers/packing.js
@@ -5,35 +5,20 @@ module.exports = {
     getPacking: async (req, res) => {
         console.log(req.user)
             try {
-            //   const user = await User.find({ user: req.user.id });
-            //   res.render("packing.ejs", { user: req.user });
               const packingItem = await Packing.find({userId:req.user.id});
               const itemsLeft = await Packing.countDocuments({userId:req.user.id,completed: false});
               res.render('packing.ejs', {items: packingItem, left: itemsLeft, user: req.user});
             } catch (err) {
               console.log(err);
             }
-          
-        // try{
-        //     const packingItems = await Packing.findOne({userId:req.user.id})
-        //     const itemsLeft = await Packing.countDocuments({userId:req.user.id,completed: false})
-        //     res.render('packing.ejs', {Items: packingItems, left: itemsLeft, user: req.user})
-        // }catch(err){
-        //     console.log(err)
-        // }
     },
     createItem: async (req, res)=>{
         try{
-            await Packing.create({item: req.body.packingItem, category: req.body.category, completed: false, userId: req.user.id})
-            console.log('Item has been added!')
-            res.redirect('/packing')
-        }catch(err){
-            console.log(err)
-        }
-    },
-    selectItem: async (req, res)=>{
-        try{
-            await Packing.create({item: req.body.selectItem, category: req.body.submit, completed: false, userId: req.user.id})
+            await Packing.create({
+                item: req.body.packingItem,
+                category: req.body.category,
+                completed: false,
+                userId: req.user.id})
             console.log('Item has been added!')
             res.redirect('/packing')
         }catch(err){
