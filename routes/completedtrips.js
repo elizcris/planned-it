bfaba5f4a8822e2b364eea4d3d62cb6c4a04d071
Elizabeth Cristina <lizzy.cristina@gmail.com>
2024-02-14 15:45:36 -0600

    Delete completed and current trip routes, controllers, ejs

--- routes/completedtrips.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const completedtripsController = require("../controllers/completedtrips");
-const { ensureAuth, ensureGuest } = require("../middleware/auth");
-
-router.get("/", ensureAuth, completedtripsController.getCompletedtrips);
-
-
-module.exports = router;
--- routes/currenttrip.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const currenttripController = require("../controllers/currenttrip");
-const { ensureAuth, ensureGuest } = require("../middleware/auth");
-
-router.get("/", ensureAuth, currenttripController.getCurrenttrip);
-
-
-module.exports = router;
